9d12164d962d839b55db835a68bd416f057cb6a5
Binara <Bsewmina@gmail.com>
2022-10-09 17:49:15 +0530

    get_fare fuction refactored

--- routeplanner/helpers.py
+++ routeplanner/helpers.py
@@ -8,6 +8,8 @@ from plotly.graph_objs import *
 
 
 route_177 = { 
+	
+	0:0,
 	1: 34, 
 	2: 42, 
 	3: 54, 
@@ -26,12 +28,16 @@ route_177 = {
 }
 
 route_176 = { 
+
+	0:0,
 	1: 34, 
 	2: 42, 
 	3: 54, 
 }
 
 route_8717 = {  
+
+	0:0,
 	1: 20, 
 	2: 20, 
 	3: 20, 
@@ -40,7 +46,7 @@ route_8717 = {
 }
 
 map_test = {
-	0: {'pos': (6.911034627182109, 79.84918916006576), 'connections': [1], 'name' : 'Kollupitiya','type': 0,'routeNo': 177, 'hValue': 1}, 
+	0: {'pos': (6.911034627182109, 79.84918916006576), 'connections': [1,16], 'name' : 'Kollupitiya','type': 0,'routeNo': 177, 'hValue': 1}, 
 	1: {'pos': (6.911751932322411, 79.86194701315071), 'connections': [0,2], 'name' : 'Viharamahadevi Park', 'type': 0, 'routeNo': 177, 'hValue': 1}, 
 	2: {'pos': (6.911385550864001, 79.87682791026592), 'connections': [1,3], 'name' : 'House Of Fashion', 'type': 0, 'routeNo': 177, 'hValue': 1}, 
 	3: {'pos': (6.911031363415147, 79.88498429384545), 'connections': [2,4], 'name' : 'Castle Street', 'type': 0, 'routeNo': 177, 'hValue': 1}, 
