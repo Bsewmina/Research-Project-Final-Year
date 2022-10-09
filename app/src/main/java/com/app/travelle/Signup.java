693d76c3f39dabaa1ee4be2ad6be96f608a0e62f
IT19179568 <it19179568@my.sliit.lk>
2022-10-09 23:26:34 +0530

    Pre-Finalization V1.0

--- app/src/main/java/com/app/travelle/Signup.java
+++ app/src/main/java/com/app/travelle/Signup.java
@@ -2,11 +2,16 @@ package com.app.travelle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class Signup extends AppCompatActivity {
-
+    private Button direct_to_login;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,5 +19,17 @@ public class Signup extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_signup);
+
+        direct_to_login = (Button) findViewById(R.id.login_direct);
+        direct_to_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                direct_toLogin();
+            }
+        });
+    }
+    public void direct_toLogin(){
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
     }
 }
